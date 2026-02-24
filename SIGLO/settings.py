fbb6b7e04ec9dc7f1729be1537f8a94812514bd3
emilymontec <emilymonterrosa.c@gmail.com>
2026-02-23 22:47:29 -0500

    Preparacion deploy

--- SIGLO/settings.py
+++ SIGLO/settings.py
@@ -58,6 +58,7 @@ DEFAULT_FROM_EMAIL = 'no-reply@siglo.local'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 import os
 
 MEDIA_URL = '/media/'
